735dd2bf7185581a0622a4c68d32f2d725f7ab12
HaroldBenoit <harold_benoit@hotmail.ch>
2021-05-19 00:07:25 +0200

    save

--- GHW2_copy.py
+++ GHW2_copy.py
@@ -7,7 +7,6 @@ Created on Tue May 18 22:59:12 2021
 
 import numpy as np
 
-codebook = {0:np.array((1,1))}
 
 def main():
     input= 'asda@¦#°#@°@§#@#§hfasugvbvavàé¨é'
@@ -36,38 +35,69 @@ def easyChannel(chanInput):
     return chanInput
     
 """
-Takes an input in [0,255] and creates the corresponding 2 times repeated codeword
+Takes an input in [0,3] and returns the corresponding codeword
 
-Example : create_code(1) gives c_1+c_1 where c_1 = (0,1,0,..0) with 255 zeros and only one 1.
 """
 def create_code(i):
-    arr = np.zeros(256)
-    arr[i] = 1
-    return np.hstack((arr,arr))
+    codebook = {0:[1,1,1],1:[1,-1,-1],2:[-1,1,-1],3:[-1,-1,1]}
+    return np.array(codebook[i])
+
+
+
+def get2bits(num):
+     bits = []
+     bits.append(num >> 6) ## get first 2 bits
+     bits.append((num & 48) >> 4) # get second 2 bits
+     bits.append((num & 12) >> 2) 
+     bits.append(num & 3)
+
+     return bits
 
 """
-Takes a utf-8 string of length k and outputs the corresponding encoding of length 2k*256 
 
-Example: encode('ab') = c_a+c_a+c_b+c_b 
 """
 def encode(input):
     arr = np.array(bytearray(input, 'utf-8')).astype('int')
     output = np.empty
     for i in range(len(arr)):
-        output = np.hstack((output,create_code(arr[i])))
+        num = arr[i]
+        bits = get2bits(num)
+        for j in range(4):
+            output = np.hstack((output,create_code(bits[j])))
         
-        #remove empty in the beginning
+    #remove empty in the beginning
     return output[1:]
+
+def recover_codeword(arr):
+    if(len(arr) != 3):
+        raise ValueError("array doesn't have length 3")
+        
+    if arr[0] == 0:
+        if arr[1] == 1:
+            if arr[2] == 1:
+                return 0
+            else:
+                return 2
+        else:
+            if arr[2] == 1:
+                return 3
+            else:
+                return 1
+            
+    elif arr[1] == 1:
+        
+    else:
+        
 
 """
 This function takes a 1D np.array of size 2k*256 such that the array represents
 2 times repeated codewords of length 256.  
 """
-def decode(input):
+def decode_noiseless(input):
     output = []
     
-    for i in range(0,input.size,512):
-        value=np.nonzero(input[i:i+512])[0][0]
+    for i in range(0,len(input),3):
+        
         
         if value < 256:
             output.append(value)
