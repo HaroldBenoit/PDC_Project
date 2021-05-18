cdacec7904f44693061156eb0b9da26f38030c08
HaroldBenoit <harold_benoit@hotmail.ch>
2021-05-18 13:04:24 +0200

    add encoding and decoding

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import numpy as np
 
 
 def main():
-    string2bits('hello')
-    
+    input= '!@@@@@@#°§¬|¢¢0idashfia'
+    encoded = string2real(input)
+    decoded= real2string(encoded)
     
 def channel(chanInput):
     chanInput = np.clip(chanInput,-1,1)
@@ -20,8 +21,28 @@ def channel(chanInput):
     return chanInput + np.sqrt(10)*np.random.randn(len(chanInput))
 
 
-def string2bits(input):
-    arr = np.array(bytearray(input, 'utf-8'))
+def string2real(input):
+    print('\n@@@@@@@@@@@@@@@@@@@@@@@ENCODING@@@@@@@@@@@@@@@@@@@@@@@\n')
+    
+    print('starting string :',input,'\n')
+    
+    arr = np.array(bytearray(input, 'utf-8')).astype('float64')
     print(arr)
+    arr = (arr -128)
+    print(arr)
+    arr = arr/255
+    print(arr)
+    return arr
+
+    
+
+def real2string(arr):
+    print('\n@@@@@@@@@@@@@@@@@@@@@@@DECODING@@@@@@@@@@@@@@@@@@@@@@@\n')
+    arr = ((arr*255) + 128).astype('byte')
+    print(arr)
+    arr = bytearray(arr)
+    print(arr,'\n')
+    string= str(arr,'utf-8')
+    print('result string:',string)
     
 main()
